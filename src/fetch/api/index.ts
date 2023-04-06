f5bd4ee97be4c2e86f8c3a300ac3b7bdb9c31036
coderbaozi <YHbaozi1988@163.com>
2023-04-06 16:01:33 +0800

    :featuer: add api declaer

--- src/fetch/api/index.ts
+++ src/fetch/api/index.ts
@@ -5,11 +5,19 @@ function getTags() {
 }
 
 interface IArticleParam {
-  tagId: number
+  tagId: number | string
   page: number
 }
 function getArticleList(param: IArticleParam) {
   return fetcher.get({ url: '/getArticleList', params: param })
 }
 
-export { getTags, getArticleList }
+function getUserName(userId: number) {
+  return fetcher.get({ url: '/getUserName', params: { userId } })
+}
+
+function getArticleInfo(articleId: string) {
+  return fetcher.get({ url: '/getArticle', params: { articleId } })
+}
+
+export { getTags, getArticleList, getUserName, getArticleInfo }
