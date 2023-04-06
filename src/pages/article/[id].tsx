dab0f0b1d620ca6c9884c7b270eee882b6a33722
coderbaozi <YHbaozi1988@163.com>
2023-04-06 16:03:21 +0800

    :feat: list page construct

--- src/pages/article/[id].tsx
+++ src/pages/article/[id].tsx
@@ -1,15 +1,18 @@
 import React, { ReactNode } from 'react'
 import DefaultLayout from '@/layouts/default'
 import Head from 'next/head'
-import { ITag } from '@/types'
-import { getTags } from '@/fetch/api'
+import { ITag, IArticle } from '@/types'
+import { getTags, getArticleList } from '@/fetch/api'
+import ArticleCard from '@/components/ArticleCard'
+import { GetServerSidePropsContext } from 'next'
 interface IProps {
   children?: ReactNode
   tags: ITag[]
+  articleList: IArticle[]
 }
 
 export const TagContext = React.createContext([] as ITag[])
-const ArticlePage: React.FC<IProps> = ({ tags }) => {
+const ArticlePage: React.FC<IProps> = ({ tags, articleList }) => {
   return (
     <>
       <TagContext.Provider value={tags}>
@@ -20,17 +23,34 @@ const ArticlePage: React.FC<IProps> = ({ tags }) => {
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
           </Head>
+          <ul className="flex flex-col justify-center items-center divide-y-2 divide-gray-200">
+            {articleList.map((article) => {
+              return (
+                <li className="mt-auto w-1/2" key={article.articleId}>
+                  <ArticleCard article={article} />
+                </li>
+              )
+            })}
+          </ul>
         </DefaultLayout>
       </TagContext.Provider>
     </>
   )
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { id } = context.query as { id: string }
   const tags = await getTags()
+  const articleList: IArticle[] = await getArticleList({ tagId: id, page: 1 })
+  // create a hashTable to prase the userid to username
+  articleList.forEach((article) => {
+    const res = article.createTime.match(/\d{4}-\d{2}-\d{2}/g)
+    article.createTime = res![0]
+  })
   return {
     props: {
       tags,
+      articleList,
     },
   }
 }
