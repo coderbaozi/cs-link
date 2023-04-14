20bf66280f50faa7f472c5d1476fdc12c567f7c1
coderbaozi <YHbaozi1988@163.com>
2023-04-14 20:23:29 +0800

    :types: type declare

---
+++ src/constants/token.ts
@@ -0,0 +1,2 @@
+export const SITE_TOKEN = 'cslinkToken'
+export const USER_INFO = 'userInfo'
---
+++ src/types/comment.d.ts
@@ -0,0 +1,12 @@
+export interface Comment {
+  articleId: number
+  userId: number
+  id: number
+  articleType: number
+  content: string
+  formUid: number
+  toUid: number
+  commentTime: string
+  username: string
+  toUserName: string
+}
