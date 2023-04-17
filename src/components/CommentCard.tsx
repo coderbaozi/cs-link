043a6f2cdf16c8b24861d5013b32d9ea34f3a710
coderbaozi <YHbaozi1988@163.com>
2023-04-17 18:08:20 +0800

    :style: modify comment style

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ReactNode } from 'react'
 import { Comment } from '@/types/comment.d'
 import Link from 'next/link'
 import { Icon } from '@iconify/react'
-import Tiptap from './TipTap'
 interface IProps {
   children?: ReactNode
   comment: Comment
@@ -11,24 +10,41 @@ interface IProps {
 
 const CommentCard: React.FC<IProps> = ({ comment }) => {
   const [status, setStatus] = useState(-1)
+  useEffect(() => {
+    console.log(comment)
+  }, [])
   return (
     <div>
-      <h2 className="mb-6">全部评论</h2>
-      <div className="w-full p-3 flex text-xl justify-between">
-        <Link href={`#`} className=" hover:underline hover:decoration-1">
-          {comment.username}
-        </Link>
-        <span className="text-base text-gray-600">{`1天前`}</span>
-      </div>
-      <p className="font-mono text-xl p-6">{comment.content}</p>
-      <div className="flex space-x-3 text-base px-6">
-        <span className="flex items-center cursor-pointer hover:text-amber-900">
-          <Icon className="mr-1" icon="solar:like-broken" /> {`点赞`}
-        </span>
-        <span className="flex items-center cursor-pointer hover:text-amber-900">
-          <Icon className="mr-1" icon="mdi:message-processing-outline" />
-          {`评论`}
-        </span>
+      <div className="w-full flex flex-row mt-10">
+        <img
+          src="https://i.postimg.cc/dtCQKRff/images.jpg"
+          className="inline w-16 h-16 rounded-full mr-6"
+          alt="avater"></img>
+        <div className="flex w-full flex-col flex-wrap">
+          <div className="flex space-x-2 mb-2">
+            <Link href={`#`} className="font-bold hover:underline hover:decoration-1">
+              {comment.username}
+            </Link>
+            <div className="relative text-slate-500">
+              <p className="absolute -top-1">.</p>
+            </div>
+            <span className="text-slate-500">{`1天前`}</span>
+          </div>
+
+          <p className="font-mono text-xl break-all mb-2">{comment.content}</p>
+
+          <div className="flex space-x-4 text-base">
+            <span className="cursor-pointer hover:text-amber-900">
+              <Icon icon="solar:like-broken" />
+            </span>
+            <span className="cursor-pointer hover:text-amber-900">
+              <Icon icon="ant-design:like-outlined" rotate={2} />
+            </span>
+            <span className="cursor-pointer hover:text-amber-900">
+              <Icon icon="mdi:message-processing-outline" />
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   )
